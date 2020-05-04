268a7d0827f8c44a4c7acecaade6737cf370162b
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-05-04 16:56:34 +0300

    Remove commented-out code

--- interactions.go
+++ interactions.go
@@ -154,13 +154,3 @@ func unmarshalAction(r json.RawMessage, callbackAction action) (action, error) {
 	}
 	return callbackAction, nil
 }
-// // MarshalJSON marshalls internal arrays
-// func (a ActionCallbacks) MarshalJSON() ([]byte, error) {
-// 	if len(a.AttachmentActions) > 0 {
-// 		return json.Marshal(&a.AttachmentActions)
-// 	} else if len(a.BlockActions) > 0 {
-// 		return json.Marshal(&a.BlockActions)
-// 	} else {
-// 		return json.Marshal(&[]AttachmentAction{})
-// 	}
-// }
